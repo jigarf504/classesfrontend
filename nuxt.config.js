0ce3a6f331a65734492ed6bc580a66bf3874d1ae
Jigar <jigar@devrepublic.nl>
2022-08-02 21:06:37 +0530

    changes in nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,73 +1,61 @@
 export default {
+  ssr: false,
+  target: "static",
   head: {
-    title: 'Test-Classes',
+    title: "Test-Classes",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-  css: [
-    '@/assets/css/tailwind.css'
-  ],
-  plugins: [
-    { src: '~/plugins/vee-validate.js' }
-  ],
+  css: ["@/assets/css/tailwind.css"],
+  plugins: [{ src: "~/plugins/vee-validate.js" }],
   components: true,
-  buildModules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxt/postcss8',
-    '@nuxtjs/dotenv',
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    'nuxt-material-design-icons',
-    '@nuxtjs/toast'
-  ],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxt/postcss8", "@nuxtjs/dotenv"],
+  modules: ["@nuxtjs/axios", "nuxt-material-design-icons", "@nuxtjs/toast"],
   toast: {
-    theme: 'toasted-primary',
-    position: 'bottom-center',
+    theme: "toasted-primary",
+    position: "bottom-center",
     duration: 3000,
-    containerClass: 'toast',
-    iconPack: 'mdi',
+    containerClass: "toast",
+    iconPack: "mdi",
     singleton: true,
-    icon: 'information',
+    icon: "information",
     action: [
       {
-        text: 'X',
+        text: "X",
         onClick: (e, toastObject) => {
-          toastObject.goAway(0)
-        }
-      }
-    ]
+          toastObject.goAway(0);
+        },
+      },
+    ],
   },
   axios: {
-    baseURL: process.env.BASE_URL
+    baseURL: process.env.BASE_URL,
   },
   build: {
     postcss: {
       plugins: {
         tailwindcss: {},
-        autoprefixer: {}
-      }
-    }
+        autoprefixer: {},
+      },
+    },
   },
   server: {
-    port: 3005 // default: 3000
+    port: 3005, // default: 3000
   },
   tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css',
-    configPath: 'tailwind.config.js',
+    cssPath: "~/assets/css/tailwind.css",
+    configPath: "tailwind.config.js",
     exposeConfig: false,
     config: {},
     injectPosition: 0,
-    viewer: true
-  }
-}
+    viewer: true,
+  },
+};
